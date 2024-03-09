e333791041a83fbd253233178edcc487a9c24e76
Multirious <multirious@outlook.com>
2024-03-09 20:13:36 +0700

    update tween timer

--- src/tween_timer.rs
+++ src/tween_timer.rs
@@ -40,7 +40,7 @@ pub struct Elasped {
     pub now: Duration,
     /// The previous elasped time
     pub previous: Duration,
-    /// [`RepeatStyle`] if the tween player just ended and repeated in some way.
+    /// Some if the tween timer just ended and repeated in some way.
     pub repeat_style: Option<RepeatStyle>,
 }
 
@@ -55,17 +55,23 @@ impl Elasped {
     }
 }
 
-/// State of a tween player, animation direction, and repeat configuration
+pub enum TickResult {
+    Continue,
+    Repeated,
+    AllDone,
+}
+
+/// Tween timer
 #[derive(Debug, Component, Clone, PartialEq, Eq, Hash, Reflect)]
 #[reflect(Component)]
 pub struct TweenTimer {
-    /// Stop the ticking system from updating this player.
+    /// Stop the ticking system from updating this timer.
     pub paused: bool,
     /// The current elasped time with other useful information.
     elasped: Elasped,
-    /// When this player should stop or repeat if configured.
+    /// When this timer should stop or repeat if configured.
     pub duration_limit: Duration,
-    /// Playback direction of the current player.
+    /// Ticking direction of the current timer.
     pub direction: AnimationDirection,
     /// Set speed of the playback to `speed_scale` second per second.
     pub speed_scale: Duration,
@@ -83,7 +89,7 @@ impl TweenTimer {
             ..Default::default()
         }
     }
-    /// Set the duration limit of this player
+    /// Set the duration limit of this timer
     pub fn set_duration(&mut self, duration: Duration) -> &mut Self {
         self.duration_limit = duration;
         self
@@ -129,9 +135,10 @@ impl TweenTimer {
         self.elasped
     }
 
-    /// Returns true if the tween player is actually finished for real,
-    /// accounting its repeat configuration.
-    pub fn is_finished(&self) -> bool {
+    /// Returns true if the tween timer all done.
+    /// All done meaning that there will be nore more ticking and all
+    /// configured repeat is exhausted.
+    pub fn is_all_done(&self) -> bool {
         let is_edge = match self.direction {
             AnimationDirection::Forward => {
                 self.elasped.now >= self.duration_limit
@@ -143,8 +150,171 @@ impl TweenTimer {
             }
         };
         match self.repeat {
-            Some(repeat) => repeat.is_finished() && is_edge,
+            Some(repeat) => repeat.exhausted() && is_edge,
             None => is_edge,
+        }
+    }
+
+    pub fn tick(
+        &mut self,
+        duration: Duration,
+        direction: AnimationDirection,
+    ) -> TickResult {
+        use AnimationDirection::*;
+        use RepeatStyle::*;
+        match (
+            direction,
+            self.repeat,
+            self.repeat_style.unwrap_or_default(),
+        ) {
+            (Forward, None, _) => {
+                if self.elasped.now >= self.duration_limit {
+                    self.elasped = Elasped {
+                        now: self.duration_limit,
+                        previous: self.elasped.now,
+                        repeat_style: None,
+                    };
+                    return TickResult::AllDone;
+                }
+                let new_now =
+                    (self.elasped.now + duration).min(self.duration_limit);
+                self.elasped = Elasped {
+                    now: new_now,
+                    previous: self.elasped.now,
+                    repeat_style: None,
+                };
+                TickResult::Continue
+            }
+            (Backward, None, _) => {
+                if self.elasped.now == Duration::ZERO {
+                    self.elasped = Elasped {
+                        now: Duration::ZERO,
+                        previous: self.elasped.now,
+                        repeat_style: None,
+                    };
+                    return TickResult::AllDone;
+                }
+                let new_now = self.elasped.now.saturating_sub(duration);
+                self.elasped = Elasped {
+                    now: new_now,
+                    previous: self.elasped.now,
+                    repeat_style: None,
+                };
+                TickResult::Continue
+            }
+            (Forward, Some(mut r), WrapAround) => {
+                let new_now = self.elasped.now + duration;
+                let will_wrap = new_now >= self.duration_limit;
+                if will_wrap && !r.try_advance_counter() {
+                    self.elasped = Elasped {
+                        now: self.duration_limit,
+                        previous: self.elasped.now,
+                        repeat_style: None,
+                    };
+                    return TickResult::AllDone;
+                }
+                let new_now = duration_rem(new_now, self.duration_limit);
+                self.elasped = Elasped {
+                    now: new_now,
+                    previous: self.elasped.now,
+                    repeat_style: if will_wrap {
+                        Some(WrapAround)
+                    } else {
+                        None
+                    },
+                };
+                TickResult::Repeated
+            }
+            (Backward, Some(mut r), WrapAround) => {
+                let will_wrap = duration > self.elasped.now;
+                if will_wrap && !r.try_advance_counter() {
+                    self.elasped = Elasped {
+                        now: Duration::ZERO,
+                        previous: self.elasped.now,
+                        repeat_style: None,
+                    };
+                    return TickResult::AllDone;
+                }
+                let new_now = if will_wrap {
+                    neg_duration_rem(
+                        duration - self.elasped.now,
+                        self.duration_limit,
+                    )
+                } else {
+                    self.elasped.now - duration
+                };
+                self.elasped = Elasped {
+                    now: new_now,
+                    previous: self.elasped.now,
+                    repeat_style: if will_wrap {
+                        Some(WrapAround)
+                    } else {
+                        None
+                    },
+                };
+                TickResult::Repeated
+            }
+            (Forward, Some(mut r), PingPong) => {
+                let new_now = self.elasped.now + duration;
+                let will_pingpong = new_now > self.duration_limit;
+                if will_pingpong {
+                    if !r.try_advance_counter() {
+                        self.elasped = Elasped {
+                            now: self.duration_limit,
+                            previous: self.elasped.previous,
+                            repeat_style: None,
+                        };
+                        return TickResult::AllDone;
+                    }
+                    let new_now =
+                        neg_duration_rem(new_now, self.duration_limit);
+                    self.direction = Backward;
+                    self.elasped = Elasped {
+                        now: new_now,
+                        previous: self.elasped.now,
+                        repeat_style: Some(PingPong),
+                    };
+                    TickResult::Repeated
+                } else {
+                    self.elasped = Elasped {
+                        now: new_now,
+                        previous: self.elasped.now,
+                        repeat_style: None,
+                    };
+                    TickResult::Continue
+                }
+            }
+            (Backward, Some(mut r), PingPong) => {
+                let will_pingpong = duration > self.elasped.now;
+                if will_pingpong {
+                    if !r.try_advance_counter() {
+                        self.elasped = Elasped {
+                            now: Duration::ZERO,
+                            previous: self.elasped.previous,
+                            repeat_style: None,
+                        };
+                        return TickResult::AllDone;
+                    }
+                    let new_now = duration_rem(
+                        duration - self.elasped.now,
+                        self.duration_limit,
+                    );
+                    self.direction = Forward;
+                    self.elasped = Elasped {
+                        now: new_now,
+                        previous: self.elasped.now,
+                        repeat_style: Some(PingPong),
+                    };
+                    TickResult::Repeated
+                } else {
+                    self.elasped = Elasped {
+                        now: self.elasped.now - duration,
+                        previous: self.elasped.now,
+                        repeat_style: None,
+                    };
+                    TickResult::Continue
+                }
+            }
         }
     }
 }
@@ -200,7 +370,10 @@ impl Repeat {
             times_repeated: 0,
         }
     }
-    pub fn is_finished(&self) -> bool {
+
+    /// Returns if all repeat has been exhausted.
+    /// Infinite repeat always returns false.
+    pub fn exhausted(&self) -> bool {
         match self {
             Repeat::Infinitely => false,
             Repeat::InfinitelyCounted { .. } => false,
@@ -231,25 +404,15 @@ impl Repeat {
     }
 }
 
-/// Tween repeat behavior
+/// Tween timer repeat behavior
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Reflect)]
 pub enum RepeatStyle {
-    /// When the direction is forward is reached the duration limit:
-    ///  - go to second 0
-    /// When the direction is backward is reached second 0:
-    ///  - go to duration limit
-    ///
-    /// Basically integer wrap around, you get it.
     #[default]
     WrapAround,
-    /// When the direction is forward is reached the duration limit:
-    ///  - Change the direction to backward
-    /// When the direction is backward is reached second 0:
-    ///  - Change the direction to forward
     PingPong,
 }
 
-/// Specfy which way the tween player is playing
+/// Specfy which way the tween timer is ticking
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Reflect)]
 pub enum AnimationDirection {
     /// Playing forward
@@ -259,7 +422,7 @@ pub enum AnimationDirection {
     Backward,
 }
 
-/// Event that emitted when a tween player just ended. This will be emitted for
+/// Event that emitted when a tween timer just ended. This will be emitted for
 /// the one that just repeated as well.
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Event, Reflect)]
 pub struct TweenTimerEnded {
@@ -268,16 +431,17 @@ pub struct TweenTimerEnded {
     /// Currently direction. If is [`RepeatStyle::PingPong`], the current direction
     /// will be its already changed direction.
     pub current_direction: AnimationDirection,
-    /// The repeat this tween player had.
+    /// The repeat this tween timer had.
     pub with_repeat: Option<Repeat>,
 }
 
 impl TweenTimerEnded {
-    /// Returns true if the tween player is actually finished for real,
-    /// accounting its repeat configuration.
-    pub fn is_finished(&self) -> bool {
+    /// Returns true if the tween timer all done.
+    /// All done meaning that there will be nore more ticking and all
+    /// configured repeat is exhausted.
+    pub fn is_all_done(&self) -> bool {
         self.with_repeat
-            .map(|repeat| repeat.is_finished())
+            .map(|repeat| repeat.exhausted())
             .unwrap_or(true)
     }
 }
@@ -288,16 +452,14 @@ pub fn tick_tween_timer_system(
     mut q_timer: Query<(Entity, &mut TweenTimer)>,
     mut ended_writer: EventWriter<TweenTimerEnded>,
 ) {
-    use AnimationDirection::*;
-    use RepeatStyle::*;
     let delta = time.delta();
     q_timer.iter_mut().for_each(|(entity, mut timer)| {
         if timer.paused {
             return;
         }
 
-        let is_prev_finished = timer.is_finished();
-        if is_prev_finished {
+        let is_prev_completed = timer.is_all_done();
+        if is_prev_completed {
             return;
         }
 
@@ -305,173 +467,18 @@ pub fn tick_tween_timer_system(
             delta.as_secs_f32() * timer.speed_scale.as_secs_f32(),
         );
 
-        let (is_now_finished, is_repeated) = 'm: {
-            match (
-                timer.direction,
-                timer.repeat,
-                timer.repeat_style.unwrap_or_default(),
-            ) {
-                (Forward, None, _) => {
-                    if timer.elasped.now >= timer.duration_limit {
-                        timer.elasped = Elasped {
-                            now: timer.duration_limit,
-                            previous: timer.elasped.now,
-                            repeat_style: None,
-                        };
-                        break 'm (true, false);
-                    }
-                    let new_now =
-                        (timer.elasped.now + delta).min(timer.duration_limit);
-                    timer.elasped = Elasped {
-                        now: new_now,
-                        previous: timer.elasped.now,
-                        repeat_style: None,
-                    };
-                    (false, false)
-                }
-                (Backward, None, _) => {
-                    if timer.elasped.now == Duration::ZERO {
-                        timer.elasped = Elasped {
-                            now: Duration::ZERO,
-                            previous: timer.elasped.now,
-                            repeat_style: None,
-                        };
-                        break 'm (true, false);
-                    }
-                    let new_now = timer.elasped.now.saturating_sub(delta);
-                    timer.elasped = Elasped {
-                        now: new_now,
-                        previous: timer.elasped.now,
-                        repeat_style: None,
-                    };
-                    (false, false)
-                }
-                (Forward, Some(mut r), WrapAround) => {
-                    let new_now = timer.elasped.now + delta;
-                    let will_wrap = new_now >= timer.duration_limit;
-                    if will_wrap && !r.try_advance_counter() {
-                        timer.elasped = Elasped {
-                            now: timer.duration_limit,
-                            previous: timer.elasped.now,
-                            repeat_style: None,
-                        };
-                        break 'm (true, false);
-                    }
-                    let new_now = duration_rem(new_now, timer.duration_limit);
-                    timer.elasped = Elasped {
-                        now: new_now,
-                        previous: timer.elasped.now,
-                        repeat_style: if will_wrap {
-                            Some(WrapAround)
-                        } else {
-                            None
-                        },
-                    };
-                    (false, true)
-                }
-                (Backward, Some(mut r), WrapAround) => {
-                    let will_wrap = delta > timer.elasped.now;
-                    if will_wrap && !r.try_advance_counter() {
-                        timer.elasped = Elasped {
-                            now: Duration::ZERO,
-                            previous: timer.elasped.now,
-                            repeat_style: None,
-                        };
-                        break 'm (true, false);
-                    }
-                    let new_now = if will_wrap {
-                        neg_duration_rem(
-                            delta - timer.elasped.now,
-                            timer.duration_limit,
-                        )
-                    } else {
-                        timer.elasped.now - delta
-                    };
-                    timer.elasped = Elasped {
-                        now: new_now,
-                        previous: timer.elasped.now,
-                        repeat_style: if will_wrap {
-                            Some(WrapAround)
-                        } else {
-                            None
-                        },
-                    };
-                    (false, true)
-                }
-                (Forward, Some(mut r), PingPong) => {
-                    let new_now = timer.elasped.now + delta;
-                    let will_pingpong = new_now > timer.duration_limit;
-                    if will_pingpong {
-                        if !r.try_advance_counter() {
-                            timer.elasped = Elasped {
-                                now: timer.duration_limit,
-                                previous: timer.elasped.previous,
-                                repeat_style: None,
-                            };
-                            break 'm (true, false);
-                        }
-                        let new_now =
-                            neg_duration_rem(new_now, timer.duration_limit);
-                        timer.direction = Backward;
-                        timer.elasped = Elasped {
-                            now: new_now,
-                            previous: timer.elasped.now,
-                            repeat_style: Some(PingPong),
-                        };
-                        (false, true)
-                    } else {
-                        timer.elasped = Elasped {
-                            now: new_now,
-                            previous: timer.elasped.now,
-                            repeat_style: None,
-                        };
-                        (false, false)
-                    }
-                }
-                (Backward, Some(mut r), PingPong) => {
-                    let will_pingpong = delta > timer.elasped.now;
-                    if will_pingpong {
-                        if !r.try_advance_counter() {
-                            timer.elasped = Elasped {
-                                now: Duration::ZERO,
-                                previous: timer.elasped.previous,
-                                repeat_style: None,
-                            };
-                            break 'm (true, false);
-                        }
-                        let new_now = duration_rem(
-                            delta - timer.elasped.now,
-                            timer.duration_limit,
-                        );
-                        timer.direction = Forward;
-                        timer.elasped = Elasped {
-                            now: new_now,
-                            previous: timer.elasped.now,
-                            repeat_style: Some(PingPong),
-                        };
-                        (false, true)
-                    } else {
-                        timer.elasped = Elasped {
-                            now: timer.elasped.now - delta,
-                            previous: timer.elasped.now,
-                            repeat_style: None,
-                        };
-                        (false, false)
-                    }
-                }
-            }
-        };
+        let timer_direction = timer.direction;
+        let tick_result = timer.tick(delta, timer_direction);
 
-        match (is_now_finished, is_repeated) {
-            (true, false) | (false, true) => {
+        match tick_result {
+            TickResult::Continue | TickResult::Repeated => {
                 ended_writer.send(TweenTimerEnded {
                     timer: entity,
                     current_direction: timer.direction,
                     with_repeat: timer.repeat,
                 });
             }
-            (true, true) => unreachable!(),
-            (false, false) => {}
+            TickResult::AllDone => {}
         }
     })
 }
