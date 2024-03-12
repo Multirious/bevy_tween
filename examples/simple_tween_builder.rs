cb908dd2936858e63702503e42f0079d0237954b
Multirious <multirious@outlook.com>
2024-03-12 13:03:30 +0700

    add simple_tween_builder example

---
+++ examples/simple_tween_builder.rs
@@ -0,0 +1,37 @@
+use bevy::prelude::*;
+use bevy_tween::prelude::*;
+use std::time::Duration;
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, DefaultTweenPlugins))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    let start = Vec3::new(-300., 0., 0.);
+    let end = Vec3::new(300., 0., 0.);
+    commands
+        .spawn((
+            SpriteBundle {
+                sprite: Sprite {
+                    custom_size: Some(Vec2::new(100., 100.)),
+                    color: Color::WHITE,
+                    ..Default::default()
+                },
+                transform: Transform::from_translation(start),
+                ..Default::default()
+            },
+            SpanTweenPlayerBundle::new(Duration::from_secs(5)),
+        ))
+        .with_children(|c| {
+            c.build_tweens().tween(
+                ..Duration::from_secs(5),
+                EaseFunction::QuadraticInOut,
+                ComponentTween::new(interpolate::Translation { start, end }),
+            );
+        });
+}
