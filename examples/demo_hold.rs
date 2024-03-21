feb3db0cb72f602c157c3140ff1cde9aeaa4867f
Multirious <multirious@outlook.com>
2024-03-21 18:12:59 +0700

    Unused import

--- examples/demo_hold.rs
+++ examples/demo_hold.rs
@@ -2,10 +2,8 @@ use std::f32::consts::PI;
 
 use bevy::{prelude::*, window::PrimaryWindow};
 use bevy_tween::{
-    component_tween_system,
-    prelude::*,
-    resource_tween_system,
-    tween_timer::{AnimationDirection, TweenTimer}, span_tween::SpanTweener,
+    component_tween_system, prelude::*, resource_tween_system,
+    span_tween::SpanTweener, tween_timer::AnimationDirection,
 };
 use rand::prelude::*;
 
