88479758e990463a059c0222ed5bdb97952eb3a4
Multirious <multirious@outlook.com>
2024-03-21 21:38:27 +0700

    Add banner_triangle example

--- examples/animation/banner_triangle.rs
+++ examples/animation/banner_triangle.rs
@@ -10,11 +10,7 @@ fn main() {
                 primary_window: Some(Window {
                     title: "Banner triangle".to_string(),
                     resizable: false,
-                    resolution: Vec2::new(1100., 200.).into(),
-                    enabled_buttons: window::EnabledButtons {
-                        maximize: false,
-                        ..Default::default()
-                    },
+                    resolution: Vec2::new(1100., 250.).into(),
                     ..Default::default()
                 }),
                 ..Default::default()
@@ -28,20 +24,8 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
-    let square_image = asset_server.load("big_triangle.png");
+    let triangle_image = asset_server.load("big_triangle.png");
     let ease = EaseFunction::ExponentialInOut;
-
-    commands.spawn(SpriteBundle {
-        texture: bevy_text,
-        transform: Transform::from_xyz(-300., 0., 0.),
-        ..Default::default()
-    });
-
-    commands.spawn(SpriteBundle {
-        texture: tween_text,
-        transform: Transform::from_xyz(340., 10., 0.),
-        ..Default::default()
-    });
 
     // colors by https://color-hex.org/color-palettes/189
     let colors = [
@@ -55,7 +39,12 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         .iter()
         .enumerate()
         .map(|(i, color)| {
-            triangle(&mut commands, square_image.clone(), *color, i as f32)
+            triangle(
+                &mut commands,
+                triangle_image.clone(),
+                *color,
+                (i + 2) as f32,
+            )
         })
         .collect::<Vec<_>>();
 
@@ -73,6 +62,54 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             snap_rotate(c, triangles[1], secs, 7, 10., ease);
             snap_rotate(c, triangles[0], secs, 7, 12., ease);
         });
+
+    commands
+        .spawn((
+            SpatialBundle::default(),
+            SpanTweenerBundle::new(Duration::from_secs_f32(12. / 7.))
+                .with_repeat(Repeat::Infinitely)
+                .tween_here(),
+            EaseFunction::ExponentialInOut,
+            ComponentTween::new(interpolate::Translation {
+                start: Vec3::ZERO,
+                end: Vec3::new(30. * 10., 0., 0.),
+            }),
+        ))
+        .with_children(|c| {
+            let color = Color::WHITE;
+            let count = 70;
+            let height = 5.;
+            let width = 20.;
+            let spacing = 30.;
+            let x_offset =
+                -(width * count as f32 + (spacing - width) * count as f32) / 2.;
+            for i in 0..count {
+                let i = i as f32;
+                c.spawn(SpriteBundle {
+                    sprite: Sprite {
+                        color,
+                        custom_size: Some(Vec2::new(width, height)),
+                        ..Default::default()
+                    },
+                    transform: Transform::from_xyz(
+                        i * spacing + x_offset,
+                        0.,
+                        0.,
+                    ),
+                    ..Default::default()
+                });
+            }
+        });
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(250., 250.)),
+            color: Color::rgb_u8(43, 44, 47),
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(0., 0., 1.),
+        ..Default::default()
+    });
 }
 
 fn snap_rotate(
