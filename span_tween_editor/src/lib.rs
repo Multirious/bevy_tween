76a8e550fdea3058a43140fb34ab10dd8a9fcfbe
Multirious <multirious@outlook.com>
2024-04-09 22:59:37 +0700

    mmhm

--- span_tween_editor/src/lib.rs
+++ span_tween_editor/src/lib.rs
@@ -1,4 +1,9 @@
-use bevy::{prelude::*, utils::HashSet};
+use std::any::TypeId;
+
+use bevy::{
+    prelude::*,
+    utils::{HashMap, HashSet},
+};
 use bevy_egui::{
     egui::{self, Widget},
     EguiContexts,
@@ -6,9 +11,11 @@ use bevy_egui::{
 use bevy_tween::{
     prelude::*,
     span_tween::{SpanTweener, TweenTimeSpan},
+    tween::TweenerMarker,
 };
 
 mod reflect_data;
+// use reflect_data::ReflectList;
 
 pub struct SpanTweenEditorPlugin;
 
@@ -26,25 +33,72 @@ struct EditorSetting {
 
 #[derive(Component)]
 struct EditorData {
+    playhead_drag: f32,
+    view_offset: egui::Vec2,
     vertical_scale: f32,
     horizontal_scale: f32,
-    selected_tween: Option<usize>,
-    selected_tweens: HashSet<usize>,
-    tweens: Vec<(TweenTimeSpan, Entity)>,
+    // selected_tween: Option<(usize, usize)>,
+    // selected_tweens: HashSet<(usize, usize)>,
+    tracks: Vec<Track>,
 }
 
 impl Default for EditorData {
     fn default() -> Self {
         EditorData {
+            playhead_drag: 20.,
+            view_offset: egui::Vec2::ZERO,
             vertical_scale: 1.,
             horizontal_scale: 1.,
-            selected_tween: None,
-            selected_tweens: HashSet::default(),
-            tweens: Vec::new(),
+            // selected_tween: None,
+            // selected_tweens: HashSet::default(),
+            tracks: Vec::default(),
         }
     }
 }
 
+struct Track {
+    tweens: HashMap<Entity, TweenTimeSpan>,
+    height: f32,
+    color: egui::Color32,
+}
+
+impl Default for Track {
+    fn default() -> Self {
+        Track {
+            tweens: HashMap::default(),
+            height: 10.,
+            color: egui::Color32::GRAY,
+        }
+    }
+}
+
+#[derive(Event)]
+struct ResetTrack {
+    tweener: Entity,
+}
+
+fn init_tracks(
+    mut reset_track: EventReader<ResetTrack>,
+    q_children: &Query<&Children>,
+    q_other_tweener: &Query<&TweenerMarker>,
+    q_tween: &Query<(&TweenTimeSpan, EntityRef)>,
+) {
+    for &ResetTrack { tweener } in reset_track.read() {
+        let children = q_children.get(tweener).ok();
+        let children = children
+            .iter()
+            .flat_map(|a| a.iter())
+            .filter(|c| !q_other_tweener.contains(**c));
+        let tweens = q_tween.iter_many([&tweener].into_iter().chain(children));
+
+        let mut track = Track::default();
+        for (span, entity_ref) in tweens {
+            track.tweens.insert(entity_ref.id(), *span);
+        }
+    }
+}
+
+#[allow(clippy::too_many_arguments)]
 fn editor_system(
     mut commands: Commands,
     mut contexts: EguiContexts,
@@ -69,12 +123,17 @@ fn editor_system(
                 timer_setting(&mut tweener, ui);
                 match editor_data {
                     Some(mut editor_data) => {
-                        timing(&mut editor, &mut editor_data, &mut tweener, ui);
+                        tweens_ui(
+                            &mut editor,
+                            &mut editor_data,
+                            &mut tweener,
+                            ui,
+                        );
                     }
                     None => {
-                        commands
-                            .entity(tweener_entity)
-                            .insert(EditorData::default());
+                        commands.entity(tweener_entity).insert(EditorData {
+                            ..Default::default()
+                        });
                     }
                 }
             }
@@ -144,86 +203,146 @@ fn timer_setting(tweener: &mut SpanTweener, ui: &mut egui::Ui) {
     });
 }
 
-fn timing(
+fn tweens_ui(
     editor: &mut EditorSetting,
     editor_data: &mut EditorData,
     tweener: &mut SpanTweener,
     ui: &mut egui::Ui,
 ) {
-    egui::TopBottomPanel::top("timing").show_inside(ui, |ui| {
-        let mut slider_now = tweener.timer.elasped().now;
-        let timer_length = tweener.timer.length.as_secs_f32();
-        ui.add(
-            egui::Slider::new(&mut slider_now, (0.)..=timer_length)
-                .handle_shape(egui::style::HandleShape::Rect {
-                    aspect_ratio: 0.1,
-                }),
-        );
-        if slider_now != tweener.timer.elasped().now {
-            tweener.timer.set_tick(slider_now);
-        }
-
-        egui::CentralPanel::default().show_inside(ui, |ui| {
-            ui.add(TweensUi {
-                now: &mut 0.,
+    egui::CentralPanel::default()
+        .frame(
+            egui::Frame::central_panel(ui.style())
+                .fill(ui.style().visuals.widgets.open.weak_bg_fill),
+        )
+        .show_inside(ui, |ui| {
+            let mut now = tweener.timer.elasped().now;
+            TweensUi {
+                playhead: &mut now,
+                length: tweener.timer.length.as_secs_f32(),
                 editor_data,
-            });
+            }
+            .ui(ui);
         });
-    });
 }
 
 struct TweensUi<'a> {
-    now: &'a mut f32,
+    playhead: &'a mut f32,
+    length: f32,
     editor_data: &'a mut EditorData,
 }
 
-impl<'a> Widget for TweensUi<'a> {
+impl<'a> TweensUi<'a> {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        let TweensUi { now, editor_data } = self;
+        let TweensUi {
+            playhead,
+            length,
+            editor_data,
+        } = self;
+        ui.with_layout(egui::Layout::top_down(egui::Align::Min), |ui| {
+            const SCALE: f32 = 100.;
+            const HEIGHT: f32 = 15.;
 
-        ui.with_layout(
-            egui::Layout::top_down_justified(egui::Align::Min),
-            |ui| {
+            let response = ui.allocate_response(
+                egui::Vec2::new(length * SCALE, HEIGHT),
+                egui::Sense {
+                    click: true,
+                    drag: true,
+                    focusable: false,
+                },
+            );
+
+            let rect = response.rect;
+
+            let playhead_x = rect.left() + *playhead * SCALE;
+
+            timeline(rect.min, HEIGHT, length, SCALE, ui);
+
+            for track_ in &editor_data.tracks {
                 let response = ui.allocate_response(
-                    egui::Vec2::new(10., 10.),
+                    egui::vec2(length * SCALE, track_.height),
                     egui::Sense {
                         click: true,
                         drag: true,
                         focusable: false,
                     },
                 );
-                let timing_head_x = 0.;
-                ui.painter().rect(egui::Rect {
-                    min: ,
-                    max: 
-                }, , , )
-                response
-            },
-        );
+                let rect = response.rect;
+                track(rect.min, length, SCALE, track_, ui);
+            }
 
-        let response = ui.allocate_response(
-            ui.available_size(),
-            egui::Sense {
-                click: true,
-                drag: true,
-                focusable: false,
-            },
+            ui.painter().line_segment(
+                [
+                    egui::pos2(playhead_x, rect.top()),
+                    egui::pos2(playhead_x, rect.bottom()),
+                ],
+                (1., egui::Color32::WHITE),
+            );
+
+            response
+        })
+        .response
+    }
+}
+
+fn timeline(
+    pos: egui::Pos2,
+    height: f32,
+    length: f32,
+    scale: f32,
+    ui: &mut egui::Ui,
+) {
+    ui.painter().rect_filled(
+        egui::Rect::from_min_max(pos, pos + egui::vec2(length * scale, height)),
+        0.,
+        ui.style().visuals.widgets.noninteractive.bg_fill,
+    );
+
+    let max_tick = (length * 8.).ceil() as i32;
+
+    for i in 0..max_tick {
+        let (brightness, shortness) = match i % 8 {
+            0 => (130, height * 0.8),
+            4 => (120, height * 0.6),
+            2 | 6 => (110, height * 0.5),
+            1 | 3 | 5 | 7 => (100, height * 0.45),
+            _ => unreachable!(),
+        };
+        let tick_x = pos.x + (i as f32 / 8.) * scale;
+        ui.painter().line_segment(
+            [
+                egui::pos2(tick_x, pos.y),
+                egui::pos2(tick_x, height - shortness),
+            ],
+            (1., egui::Color32::from_gray(brightness)),
         );
-        if response.hovered() {
-            ui.painter().rect(
-                response.rect.with_max_x(response.rect.max.x - 10.),
-                0.,
-                egui::Color32::WHITE,
-                (0., egui::Color32::WHITE),
-            );
-        } else {
-            ui.painter().rect(
-                response.rect,
-                0.,
-                egui::Color32::WHITE,
-                (0., egui::Color32::WHITE),
-            );
-        }
-        response
+    }
+}
+
+fn track(
+    pos: egui::Pos2,
+    length: f32,
+    scale: f32,
+    track: &Track,
+    ui: &mut egui::Ui,
+) {
+    ui.painter().rect_filled(
+        egui::Rect::from_min_max(
+            pos,
+            pos + egui::Vec2::new(length * scale, track.height),
+        ),
+        0.,
+        egui::Color32::WHITE,
+    );
+    for span in track.tweens.values() {
+        let min = span.min().duration().as_secs_f32();
+        let max = span.max().duration().as_secs_f32();
+        ui.painter().rect_filled(
+            egui::Rect::from_min_max(
+                egui::pos2(min * scale, pos.y),
+                egui::pos2(max * scale, pos.y + track.height),
+            ),
+            0.,
+            track.color,
+        );
     }
 }
--- span_tween_editor/src/reflect_data.rs
+++ span_tween_editor/src/reflect_data.rs
@@ -1,1 +1,25 @@
+use super::*;
+use bevy::reflect::FromType;
+use bevy_tween::interpolate::Translation;
 
+// fn register(app: &mut App) {
+//     app.register_type_data::<Translation, ReflectTween>();
+// }
+
+// pub trait TweenUi {
+//     fn target(&mut self, ui: &mut egui::Ui);
+//     fn interpolator(&mut self, ui: &mut egui::Ui);
+// }
+
+// pub trait InterpolationUi {
+//     fn interpolation_ui(&mut self, ui: &mut egui::Ui);
+// }
+
+// #[derive(Default)]
+// pub struct ReflectList {
+//     tweens: TypeIdMap<ReflectComponent>,
+// }
+
+// impl ReflectList {
+//     fn from_type_registry(type_registry: &TypeRegistry) {}
+// }
