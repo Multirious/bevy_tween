4bdea8cc0017b7c8ecf2d82dd66a0adc869247fd
Multirious <multirious@outlook.com>
2024-06-08 21:29:40 +0700

    wha

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 use rustc_version::{version_meta, Channel};
 
 fn main() {
-    println!("cargo::rustc-check-cfg=cfg(CHANNEL_STABLE)");
-    println!("cargo::rustc-check-cfg=cfg(CHANNEL_BETA)");
-    println!("cargo::rustc-check-cfg=cfg(CHANNEL_NIGHTLY)");
-    println!("cargo::rustc-check-cfg=cfg(CHANNEL_DEV)");
+    // println!("cargo::rustc-check-cfg=cfg(CHANNEL_STABLE)");
+    // println!("cargo::rustc-check-cfg=cfg(CHANNEL_BETA)");
+    // println!("cargo::rustc-check-cfg=cfg(CHANNEL_NIGHTLY)");
+    // println!("cargo::rustc-check-cfg=cfg(CHANNEL_DEV)");
     // Set cfg flags depending on release channel
     let channel = match version_meta().unwrap().channel {
         Channel::Stable => "CHANNEL_STABLE",
