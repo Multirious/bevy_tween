7e8f5c6fc1840eea9e7cc803a2e9110ed6cef933
Multirious <multirious@outlook.com>
2024-06-08 21:52:05 +0700

    Fix build and bevy_time_runner

--- build.rs
+++ build.rs
@@ -1,16 +1,8 @@
 use rustc_version::{version_meta, Channel};
 
 fn main() {
-    // println!("cargo::rustc-check-cfg=cfg(CHANNEL_STABLE)");
-    // println!("cargo::rustc-check-cfg=cfg(CHANNEL_BETA)");
-    // println!("cargo::rustc-check-cfg=cfg(CHANNEL_NIGHTLY)");
-    // println!("cargo::rustc-check-cfg=cfg(CHANNEL_DEV)");
-    // Set cfg flags depending on release channel
-    let channel = match version_meta().unwrap().channel {
-        Channel::Stable => "CHANNEL_STABLE",
-        Channel::Beta => "CHANNEL_BETA",
-        Channel::Nightly => "CHANNEL_NIGHTLY",
-        Channel::Dev => "CHANNEL_DEV",
+    if let Channel::Nightly = version_meta().unwrap().channel {
+        println!("cargo::rustc-check-cfg=cfg(CHANNEL_NIGHTLY)");
+        println!("cargo::rustc-cfg=CHANNEL_NIGHTLY")
     };
-    println!("cargo:rustc-cfg={}", channel)
 }
