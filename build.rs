ea63b69c873c654693d597897da59702c7cb3a1a
Multirious <multirious@outlook.com>
2024-06-08 15:14:51 +0700

    Fix cfg nightly doc_auto_cfg

---
+++ build.rs
@@ -0,0 +1,16 @@
+use rustc_version::{version_meta, Channel};
+
+fn main() {
+    println!("cargo::rustc-check-cfg=cfg(CHANNEL_STABLE)");
+    println!("cargo::rustc-check-cfg=cfg(CHANNEL_BETA)");
+    println!("cargo::rustc-check-cfg=cfg(CHANNEL_NIGHTLY)");
+    println!("cargo::rustc-check-cfg=cfg(CHANNEL_DEV)");
+    // Set cfg flags depending on release channel
+    let channel = match version_meta().unwrap().channel {
+        Channel::Stable => "CHANNEL_STABLE",
+        Channel::Beta => "CHANNEL_BETA",
+        Channel::Nightly => "CHANNEL_NIGHTLY",
+        Channel::Dev => "CHANNEL_DEV",
+    };
+    println!("cargo:rustc-cfg={}", channel)
+}
