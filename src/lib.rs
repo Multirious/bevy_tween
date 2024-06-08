67d5e8dafee89d9f63593c6f1554f611c472bda4
Multirious <multirious@outlook.com>
2024-06-08 23:42:21 +0700

    Remove AnimationTarget from prelude

--- src/lib.rs
+++ src/lib.rs
@@ -291,9 +291,7 @@ pub mod prelude {
 
     pub use crate::combinator::{AnimationBuilderExt, TransformTargetStateExt};
 
-    pub use crate::tween::{
-        AnimationTarget, IntoTarget, TweenEvent, TweenEventData,
-    };
+    pub use crate::tween::{IntoTarget, TweenEvent, TweenEventData};
 
     #[cfg(feature = "bevy_asset")]
     pub use crate::tween::AssetDynTween;
