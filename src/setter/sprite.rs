9214a98b890738c8690e76f06fafe6e5d52319a1
Multirious <multirious@outlook.com>
2024-07-01 00:23:20 +0700

    Remove test plugin in setter::sprite

--- src/setter/sprite.rs
+++ src/setter/sprite.rs
@@ -11,13 +11,6 @@ impl Setter<Sprite, Color> for SpriteColor {
     }
 }
 
-fn plugin(app: &mut App) {
-    app.add_systems(
-        Update,
-        super::apply_component_tween_system::<SpriteColor, _, _>,
-    );
-}
-
 #[derive(Debug, Default, Clone, PartialEq, Component, Reflect)]
 #[reflect(Component)]
 pub struct ColorMaterial;
