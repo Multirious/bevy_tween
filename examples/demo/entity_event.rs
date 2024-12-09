f3b0df13dc2612ce292dd2d8792bafe01c9f3d55
Multirious <multirious@outlook.com>
2024-12-09 14:26:35 +0700

    Update entity_event example to use observer instead of bevy_eventlistener

--- examples/demo/entity_event.rs
+++ examples/demo/entity_event.rs
@@ -1,5 +1,4 @@
 use bevy::prelude::*;
-use bevy_eventlistener::prelude::*;
 use bevy_tween::{
     bevy_time_runner::TimeRunnerEnded,
     combinator::{event, forward, sequence},
@@ -10,35 +9,29 @@ fn main() {
     App::new()
         .add_plugins((MinimalPlugins, DefaultTweenPlugins))
         .add_systems(Startup, setup)
+        .add_observer(|trigger: Trigger<TweenEvent<&'static str>>| {
+            println!("TweenEvent: {}", trigger.data)
+        })
+        .add_observer(|trigger: Trigger<TimeRunnerEnded>| {
+            if trigger.is_completed() {
+                println!("done!");
+            } else {
+                println!("repeating");
+            }
+        })
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands
-        .spawn(On::<TimeRunnerEnded>::run(
-            |listener: Listener<TimeRunnerEnded>| {
-                if listener.is_completed() {
-                    println!("done!");
-                } else {
-                    println!("repeating");
-                }
-            },
-        ))
         .animation()
         .repeat(Repeat::times(5))
         .insert(forward(Duration::from_secs_f32(0.5)));
 
-    commands
-        .spawn(On::<TweenEvent<&'static str>>::run(
-            |listener: Listener<TweenEvent<&'static str>>| {
-                println!("{}", listener.data);
-            },
-        ))
-        .animation()
-        .insert(sequence((
-            forward(Duration::from_secs_f32(3.)),
-            event("event"),
-            forward(Duration::from_secs_f32(0.5)),
-            event("listener"),
-        )));
+    commands.animation().insert(sequence((
+        forward(Duration::from_secs_f32(3.)),
+        event("tween"),
+        forward(Duration::from_secs_f32(0.5)),
+        event("event"),
+    )));
 }
