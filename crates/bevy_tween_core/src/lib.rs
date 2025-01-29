6b7ec79901fcc3aa66a408aeab1c75d486d7bec3
Multirious <multirious@outlook.com>
2025-01-29 11:23:31 +0700

    Make argument module public and dont re-export it

--- crates/bevy_tween_core/src/lib.rs
+++ crates/bevy_tween_core/src/lib.rs
@@ -1,13 +1,12 @@
 #![allow(clippy::type_complexity)]
 
 mod alter;
-mod argument;
+pub mod argument;
 #[cfg(feature = "bevy_app")]
 mod plugin;
 mod systems;
 
 pub use alter::*;
-pub use argument::*;
 #[cfg(feature = "bevy_app")]
 pub use plugin::*;
 pub use systems::*;
