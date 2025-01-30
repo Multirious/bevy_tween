39f39a7bba1254de6be303d0627ec7cd320dd300
Multirious <multirious@outlook.com>
2025-01-30 18:10:02 +0700

    bevy_tween: Implement lib

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub use bevy_tween_core as tween_core;
+pub use bevy_tween_event as tween_event;
+pub use bevy_tween_framework as tween_framework;
+
+pub mod prelude {
+    pub use bevy_tween_framework::prelude::*;
+}
