b49271bb3dbed64ff9aa5a9d2c5d6eeabed014b9
Multirious <multirious@outlook.com>
2025-01-30 17:58:58 +0700

    framework: Add test

--- crates/bevy_tween_framework/src/lib.rs
+++ crates/bevy_tween_framework/src/lib.rs
@@ -2,3 +2,6 @@ pub mod build;
 pub mod commands;
 pub mod prelude;
 pub mod timing;
+
+#[cfg(test)]
+mod test;
---
+++ crates/bevy_tween_framework/src/test.rs
@@ -0,0 +1,132 @@
+use std::time::Duration;
+
+use crate::prelude::*;
+use bevy::{prelude::*, time::TimeUpdateStrategy};
+use bevy_ecs::system::RunSystemOnce;
+use bevy_tween_core::alters::consts::Translation;
+
+fn default_test_app() -> App {
+    let mut app = App::new();
+    app.add_plugins((
+        MinimalPlugins,
+        bevy_tween_core::DefaultTweenCorePlugins,
+        bevy_tween_core::AltererPlugin::<
+            bevy_tween_core::alters::types::Translation,
+        >::default(),
+    ));
+    *app.world_mut().resource_mut::<TimeUpdateStrategy>() =
+        TimeUpdateStrategy::ManualDuration(secs(1));
+    app.world_mut()
+        .resource_mut::<Time<Virtual>>()
+        .set_max_delta(secs(1));
+    app
+}
+
+fn secs(secs: u64) -> Duration {
+    Duration::from_secs(secs)
+}
+
+fn component<T: Component>(app: &App, id: Entity) -> &T {
+    app.world().get::<T>(id).unwrap()
+}
+
+fn elapsed(app: &App) -> Duration {
+    app.world().resource::<Time>().elapsed()
+}
+
+#[test]
+fn simple() {
+    use crate::timing::{forward, sequence};
+    let mut app = default_test_app();
+    let target = app.world_mut().spawn(Transform::IDENTITY);
+    let target_id = target.id();
+    app.world_mut()
+        .run_system_once(move |mut commands: Commands| {
+            let target = target_id.tween_via(Translation);
+            commands.animation().insert(sequence((
+                forward(secs(1)),
+                target.ease(
+                    Vec3::ZERO,
+                    Vec3::splat(4.),
+                    EaseFunction::Linear,
+                    secs(4),
+                ),
+                forward(secs(2)),
+                target.ease(
+                    Vec3::splat(8.),
+                    Vec3::splat(10.),
+                    EaseFunction::Linear,
+                    secs(2),
+                ),
+            )));
+        })
+        .unwrap();
+
+    app.update();
+    assert_eq!(elapsed(&app), secs(0));
+    assert_eq!(
+        component::<Transform>(&app, target_id).translation,
+        Vec3::splat(0.)
+    );
+    app.update();
+    assert_eq!(elapsed(&app), secs(1));
+    assert_eq!(
+        component::<Transform>(&app, target_id).translation,
+        Vec3::splat(0.)
+    );
+    app.update();
+    assert_eq!(elapsed(&app), secs(2));
+    assert_eq!(
+        component::<Transform>(&app, target_id).translation,
+        Vec3::splat(1.)
+    );
+    app.update();
+    assert_eq!(elapsed(&app), secs(3));
+    assert_eq!(
+        component::<Transform>(&app, target_id).translation,
+        Vec3::splat(2.)
+    );
+    app.update();
+    assert_eq!(elapsed(&app), secs(4));
+    assert_eq!(
+        component::<Transform>(&app, target_id).translation,
+        Vec3::splat(3.)
+    );
+    app.update();
+    assert_eq!(elapsed(&app), secs(5));
+    assert_eq!(
+        component::<Transform>(&app, target_id).translation,
+        Vec3::splat(4.)
+    );
+    app.update();
+    assert_eq!(elapsed(&app), secs(6));
+    assert_eq!(
+        component::<Transform>(&app, target_id).translation,
+        Vec3::splat(4.)
+    );
+    app.update();
+    assert_eq!(elapsed(&app), secs(7));
+    assert_eq!(
+        component::<Transform>(&app, target_id).translation,
+        Vec3::splat(8.)
+    );
+    app.update();
+    assert_eq!(elapsed(&app), secs(8));
+    assert_eq!(
+        component::<Transform>(&app, target_id).translation,
+        Vec3::splat(9.)
+    );
+    app.update();
+    assert_eq!(elapsed(&app), secs(9));
+    assert_eq!(
+        component::<Transform>(&app, target_id).translation,
+        Vec3::splat(10.)
+    );
+    app.update();
+    assert_eq!(elapsed(&app), secs(10));
+    assert_eq!(
+        component::<Transform>(&app, target_id).translation,
+        Vec3::splat(10.)
+    );
+    app.update();
+}
