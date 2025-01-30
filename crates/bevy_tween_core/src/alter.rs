5c283c64a9b6631c722dddf9652abd1434d0322c
Multirious <multirious@outlook.com>
2025-01-30 12:06:12 +0700

    core: Update Alter
    Remove `Alter::Error`
    Make `Alter::Target` requires `Eq + Hash + Clone + Send + Sync + 'static`
    Make `Alter::Value` requires `Animatable + Clone`
    Make `AlterSingle::Value` requires `Animatable + Clone`
    Change `input` parameter in `Alter::alter` to `In<impl Iterator<Item = (&'a Self::Target, &'a Self::Value)>>`
    Remove `&self` argument from `AlterSingle::alter_single`
    Remove error types
    Some other clean up

--- crates/bevy_tween_core/src/alter.rs
+++ crates/bevy_tween_core/src/alter.rs
@@ -1,32 +1,30 @@
+use bevy_animation::animatable::Animatable;
 #[cfg(feature = "bevy_asset")]
 use bevy_asset::{Asset, Assets, Handle, UntypedHandle};
+use std::hash::Hash;
 
 use bevy_ecs::{
     component::Component,
     entity::Entity,
-    query::QueryEntityError,
     system::{In, Query, ResMut, Resource, SystemParam, SystemParamItem},
 };
 
-use super::argument;
-
 pub trait Alter: Send + Sync + 'static {
-    type Target: Send + Sync + 'static;
-    type Value: Send + Sync + 'static;
+    type Target: Eq + Hash + Clone + Send + Sync + 'static;
+    type Value: Animatable + Clone;
     type Param<'w, 's>: for<'w2, 's2> SystemParam<
         Item<'w2, 's2> = Self::Param<'w2, 's2>,
     >;
-    type Error<'w>;
-    fn alter<'a>(
-        input: In<(Entity, &Self::Target, &Self::Value)>,
-        param: &'a mut SystemParamItem<Self::Param<'a, '_>>,
-    ) -> Result<(), Self::Error<'a>>;
+    fn alter<'w, 'a>(
+        input: In<impl Iterator<Item = (&'a Self::Target, &'a Self::Value)>>,
+        param: &'w mut SystemParamItem<Self::Param<'w, '_>>,
+    );
 }
 
 pub trait AlterSingle: Send + Sync + 'static {
-    type Value: Send + Sync + 'static;
+    type Value: Animatable + Clone;
     type Item: Send + Sync + 'static;
-    fn alter_single(&self, item: &mut Self::Item, value: &Self::Value);
+    fn alter_single(item: &mut Self::Item, value: &Self::Value);
 }
 
 #[derive(Debug)]
@@ -35,14 +33,6 @@ where
     T: AlterSingle,
     T::Item: Component;
 
-#[derive(Debug, thiserror::Error)]
-pub enum AlterComponentError<'w> {
-    #[error("Query source error: {0}")]
-    QuerySourceError(QueryEntityError<'w>),
-    #[error("Query component error: {0}")]
-    QueryComponentError(QueryEntityError<'w>),
-}
-
 impl<T> Alter for AlterComponent<T>
 where
     T: AlterSingle,
@@ -50,24 +40,18 @@ where
 {
     type Target = Entity;
     type Value = T::Value;
-    type Param<'w, 's> = (
-        Query<'w, 's, &'static argument::Alterer<AlterComponent<T>>>,
-        Query<'w, 's, &'static mut T::Item>,
-    );
-    type Error<'w> = AlterComponentError<'w>;
+    type Param<'w, 's> = Query<'w, 's, &'static mut T::Item>;
 
-    fn alter<'a>(
-        In((source, target, value)): In<(Entity, &Self::Target, &Self::Value)>,
-        (q_source, q_component): &'a mut SystemParamItem<Self::Param<'a, '_>>,
-    ) -> Result<(), Self::Error<'a>> {
-        let mut target_component = q_component
-            .get_mut(*target)
-            .map_err(AlterComponentError::QueryComponentError)?;
-        let alterer = q_source
-            .get_mut(source)
-            .map_err(AlterComponentError::QuerySourceError)?;
-        alterer.0 .0.alter_single(&mut *target_component, value);
-        Ok(())
+    fn alter<'w, 'a>(
+        In(iter): In<impl Iterator<Item = (&'a Self::Target, &'a Self::Value)>>,
+        q_component: &'w mut SystemParamItem<Self::Param<'w, '_>>,
+    ) {
+        for (target, value) in iter {
+            let Ok(mut target_component) = q_component.get_mut(*target) else {
+                continue;
+            };
+            T::alter_single(&mut *target_component, value);
+        }
     }
 }
 
@@ -76,14 +60,6 @@ where
     T: AlterSingle,
     T::Item: Resource;
 
-#[derive(Debug, thiserror::Error)]
-pub enum AlterResourceError<'w> {
-    #[error("Query source error: {0}")]
-    QuerySourceError(QueryEntityError<'w>),
-    #[error("Resource does not exists")]
-    ResourceNotExists,
-}
-
 impl<T> Alter for AlterResource<T>
 where
     T: AlterSingle,
@@ -91,24 +67,18 @@ where
 {
     type Target = ();
     type Value = T::Value;
-    type Param<'w, 's> = (
-        Query<'w, 's, &'static argument::Alterer<AlterResource<T>>>,
-        Option<ResMut<'w, T::Item>>,
-    );
-    type Error<'w> = AlterResourceError<'w>;
+    type Param<'w, 's> = Option<ResMut<'w, T::Item>>;
 
-    fn alter<'a>(
-        In((source, (), value)): In<(Entity, &Self::Target, &Self::Value)>,
-        (q_source, resource): &'a mut SystemParamItem<Self::Param<'a, '_>>,
-    ) -> Result<(), Self::Error<'a>> {
-        let alterer = q_source
-            .get_mut(source)
-            .map_err(AlterResourceError::QuerySourceError)?;
-        let resource = resource
-            .as_mut()
-            .ok_or(AlterResourceError::ResourceNotExists)?;
-        alterer.0 .0.alter_single(&mut *resource, value);
-        Ok(())
+    fn alter<'w, 'a>(
+        In(iter): In<impl Iterator<Item = (&'a Self::Target, &'a Self::Value)>>,
+        resource: &mut SystemParamItem<Self::Param<'w, '_>>,
+    ) {
+        let Some(resource) = resource.as_mut() else {
+            return;
+        };
+        for (_, value) in iter {
+            T::alter_single(&mut *resource, value);
+        }
     }
 }
 
@@ -120,9 +90,7 @@ where
 
 #[cfg(feature = "bevy_asset")]
 #[derive(Debug, thiserror::Error)]
-pub enum AlterAssetError<'w> {
-    #[error("Query source error: {0}")]
-    QuerySourceError(QueryEntityError<'w>),
+pub enum AlterAssetError {
     #[error("Asset resource does not exists")]
     AssetResourceDoesNotExists,
     #[error("Asset {0:?} does not exists")]
@@ -137,26 +105,20 @@ where
 {
     type Target = Handle<T::Item>;
     type Value = T::Value;
-    type Param<'w, 's> = (
-        Query<'w, 's, &'static argument::Alterer<AlterAsset<T>>>,
-        Option<ResMut<'w, Assets<T::Item>>>,
-    );
-    type Error<'w> = AlterAssetError<'w>;
+    type Param<'w, 's> = Option<ResMut<'w, Assets<T::Item>>>;
 
-    fn alter<'a>(
-        In((source, target, value)): In<(Entity, &Self::Target, &Self::Value)>,
-        (q_source, resource): &'a mut SystemParamItem<Self::Param<'a, '_>>,
-    ) -> Result<(), Self::Error<'a>> {
-        let alterer = q_source
-            .get_mut(source)
-            .map_err(AlterAssetError::QuerySourceError)?;
-        let assets = resource
-            .as_mut()
-            .ok_or(AlterAssetError::AssetResourceDoesNotExists)?;
-        let asset = assets.get_mut(target).ok_or(
-            AlterAssetError::AssetDoesNotExists(target.clone_weak().untyped()),
-        )?;
-        alterer.0 .0.alter_single(asset, value);
-        Ok(())
+    fn alter<'w, 'a>(
+        In(iter): In<impl Iterator<Item = (&'a Self::Target, &'a Self::Value)>>,
+        resource: &mut SystemParamItem<Self::Param<'w, '_>>,
+    ) {
+        let Some(assets) = resource.as_mut() else {
+            return;
+        };
+        for (target, value) in iter {
+            let Some(asset) = assets.get_mut(target) else {
+                return;
+            };
+            T::alter_single(asset, value);
+        }
     }
 }
