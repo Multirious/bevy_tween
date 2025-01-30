8c2c348ba9b966760914faa5be103ea6e6d5e77b
Multirious <multirious@outlook.com>
2025-01-30 12:16:18 +0700

    core: Update systems
    Remove clean_blend_inputs_system
    Update progress_curve_system
    Add update_blend_system
    Update alterer_system

--- crates/bevy_tween_core/src/systems.rs
+++ crates/bevy_tween_core/src/systems.rs
@@ -1,9 +1,7 @@
-use std::fmt::Display;
-
+use bevy_animation::animatable::Animatable;
 use bevy_ecs::{
-    entity::Entity,
     query::With,
-    system::{Commands, In, Query},
+    system::{In, Query, Res, ResMut},
 };
 use bevy_math::{curve::Curve, FloatExt};
 
@@ -11,102 +9,80 @@ use crate::alter::Alter;
 
 use super::argument;
 
-pub fn clear_blend_inputs_system<V>(
-    mut q_values: Query<&mut argument::BlendInputs<V>>,
-) where
-    V: bevy_animation::animatable::Animatable,
-{
-    q_values
-        .par_iter_mut()
-        .for_each(|mut blend_inputs| blend_inputs.0.clear());
-}
-
 pub fn progress_curve_system<C, V>(
-    q_curve_progresses: Query<(
+    mut q_curve_progresses: Query<(
         &argument::Curve<C, V>,
-        &argument::ValueInputId,
+        &mut argument::SampledValue<V>,
         &bevy_time_runner::TimeSpanProgress,
     )>,
-    mut q_values: Query<&mut argument::BlendInputs<V>>,
 ) where
     C: Curve<V> + Send + Sync + 'static,
-    V: bevy_animation::animatable::Animatable,
+    V: Animatable,
 {
-    for (curve, values_id, progress) in &q_curve_progresses {
-        let mut values = q_values.get_mut(values_id.0).expect("Values exists");
-        let domain = curve.0.domain();
-
-        let Some(sampled) = curve
-            .0
-            .sample(domain.start().lerp(domain.end(), progress.now_percentage))
-        else {
-            continue;
-        };
-        values.0.push(bevy_animation::animatable::BlendInput {
-            value: sampled,
-            weight: 1.0,
-            additive: false,
-        });
-    }
+    q_curve_progresses.par_iter_mut().for_each(
+        |(curve, mut sampled_value, progress)| {
+            let domain = curve.0.domain();
+            let Some(value) = curve.0.sample(
+                domain.start().lerp(domain.end(), progress.now_percentage),
+            ) else {
+                return;
+            };
+            sampled_value.0 = Some(value);
+        },
+    );
 }
 
-pub fn blend_inputs_system<V>(
-    mut commands: Commands,
-    mut values: Query<(
-        Entity,
-        &argument::BlendInputs<V>,
-        Option<&mut argument::FinalValue<V>>,
-    )>,
-) where
-    V: bevy_animation::animatable::Animatable + Clone,
-{
-    for (id, blend_inputs, final_value) in &mut values {
-        if blend_inputs.0.is_empty() && final_value.is_some() {
-            commands.entity(id).remove::<argument::FinalValue<V>>();
-            continue;
-        }
-        let value = bevy_animation::animatable::Animatable::blend(
-            blend_inputs.0.iter().map(|i| {
-                bevy_animation::animatable::BlendInput {
-                    weight: i.weight,
-                    value: i.value.clone(),
-                    additive: i.additive,
-                }
-            }),
-        );
-        match final_value {
-            Some(mut final_value) => {
-                final_value.0 = value;
-            }
-            None => {
-                commands.entity(id).insert(argument::FinalValue(value));
-            }
-        }
-    }
-}
-
-pub fn alterer_system<A>(
-    q_alterers: Query<
+pub fn update_blend_system<A>(
+    mut res: ResMut<crate::TweenBlend<A>>,
+    values: Query<
         (
-            Entity,
             &argument::Target<A::Target>,
-            &argument::ValueInputId,
+            &argument::SampledValue<A::Value>,
+            Option<&argument::Blend>,
         ),
         With<argument::Alterer<A>>,
     >,
-    q_final_value: Query<&argument::FinalValue<A::Value>>,
+    mut q_final_values: Query<
+        (
+            &argument::Target<A::Target>,
+            &mut argument::FinalValue<A::Value>,
+        ),
+        With<argument::Alterer<A>>,
+    >,
+) where
+    A: Alter,
+{
+    res.clear_inputs();
+    values.iter().for_each(|(target, value, blend)| {
+        let Some(value) = &value.0 else { return };
+        let input = match blend {
+            Some(blend) => bevy_animation::prelude::BlendInput {
+                weight: blend.weigth,
+                value: value.clone(),
+                additive: blend.additive,
+            },
+            None => bevy_animation::prelude::BlendInput {
+                weight: 1.0,
+                value: value.clone(),
+                additive: false,
+            },
+        };
+        res.insert(&target.0, input);
+    });
+
+    res.blend_all_and_set_final_value();
+    q_final_values
+        .par_iter_mut()
+        .for_each(|(target, mut value)| {
+            value.0 = res.final_value(&target.0).cloned();
+        });
+}
+
+pub fn alterer_system<A>(
+    blend: Res<crate::TweenBlend<A>>,
     mut param: A::Param<'_, '_>,
 ) where
     A: Alter,
-    for<'w> A::Error<'w>: Display,
 {
-    for (source_id, target, values_id) in &q_alterers {
-        let values = q_final_value.get(values_id.0).expect("Values exists");
-        let result =
-            A::alter(In((source_id, &target.0, &values.0)), &mut param);
-        match result {
-            Ok(()) => {}
-            Err(e) => bevy_log::warn!("{e}"),
-        }
-    }
+    A::alter(In(blend.iter_targets_value()), &mut param);
 }
