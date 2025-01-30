2726de4c802e1fad6a4696aa2650f98bae10349c
Multirious <multirious@outlook.com>
2025-01-30 14:16:12 +0700

    framework: Implement builders

---
+++ crates/bevy_tween_framework/src/build.rs
@@ -0,0 +1,198 @@
+use std::hash::Hash;
+use std::time::Duration;
+
+use bevy_ecs::entity::Entity;
+use bevy_math::curve::{EaseFunction, EasingCurve};
+use bevy_time_runner::TimeSpan;
+use bevy_tween_core::{argument, Alter};
+
+use crate::commands::AnimationCommands;
+
+pub trait BuildAnimation {
+    fn build(self, commands: &mut AnimationCommands, position: &mut Duration);
+}
+
+impl<F> BuildAnimation for F
+where
+    F: FnOnce(&mut AnimationCommands, &mut Duration),
+{
+    fn build(self, commands: &mut AnimationCommands, position: &mut Duration) {
+        self(commands, position)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct BuildTween<A, C>
+where
+    A: Alter,
+{
+    pub duration: Duration,
+    pub target: A::Target,
+    pub alter: A,
+    pub curve: C,
+}
+impl<A, C> BuildTween<A, C>
+where
+    A: Alter,
+{
+    pub fn new(
+        duration: Duration,
+        target: A::Target,
+        alter: A,
+        curve: C,
+    ) -> Self {
+        BuildTween {
+            duration,
+            target,
+            alter,
+            curve,
+        }
+    }
+}
+
+impl<A, C> BuildAnimation for BuildTween<A, C>
+where
+    A: Alter,
+    C: bevy_math::Curve<A::Value> + Send + Sync + 'static,
+{
+    fn build(self, commands: &mut AnimationCommands, position: &mut Duration) {
+        let start = *position;
+        let end = *position + self.duration;
+        commands.spawn((
+            TimeSpan::try_from(start..end).unwrap(),
+            argument::Target(self.target),
+            argument::Alterer(self.alter),
+            argument::Curve::new(self.curve),
+        ));
+        *position = end;
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct TweenBuilder<Args>(Args);
+impl<T> TweenBuilder<Target<T>>
+where
+    T: Eq + Hash + Clone + Send + Sync + 'static,
+{
+    pub fn ease_via<A>(
+        &self,
+        via: A,
+        from: A::Value,
+        to: A::Value,
+        by: EaseFunction,
+        for_duration: Duration,
+    ) -> BuildTween<A, EasingCurve<A::Value>>
+    where
+        A: Alter<Target = T>,
+    {
+        BuildTween {
+            duration: for_duration,
+            target: self.0.target.clone(),
+            alter: via,
+            curve: EasingCurve::new(from, to, by),
+        }
+    }
+
+    pub fn via<A>(self, via: A) -> TweenBuilder<TargetAlter<A>>
+    where
+        A: Alter<Target = T>,
+    {
+        TweenBuilder(TargetAlter {
+            target: self.0.target,
+            alter: via,
+        })
+    }
+}
+impl<A> TweenBuilder<TargetAlter<A>>
+where
+    A: Alter + Clone,
+{
+    pub fn ease(
+        &self,
+        from: A::Value,
+        to: A::Value,
+        by: EaseFunction,
+        for_duration: Duration,
+    ) -> BuildTween<A, EasingCurve<A::Value>> {
+        BuildTween {
+            duration: for_duration,
+            target: self.0.target.clone(),
+            alter: self.0.alter.clone(),
+            curve: EasingCurve::new(from, to, by),
+        }
+    }
+
+    pub fn from(self, value: A::Value) -> TweenBuilder<TargetAlterState<A>> {
+        TweenBuilder(TargetAlterState {
+            alter: self.0.alter,
+            target: self.0.target,
+            state: value,
+        })
+    }
+}
+
+impl<A> TweenBuilder<TargetAlterState<A>>
+where
+    A: Alter + Clone,
+{
+    pub fn ease_to(
+        &mut self,
+        to: A::Value,
+        by: EaseFunction,
+        for_duration: Duration,
+    ) -> BuildTween<A, EasingCurve<A::Value>> {
+        let from = std::mem::replace(&mut self.0.state, to.clone());
+        BuildTween {
+            duration: for_duration,
+            target: self.0.target.clone(),
+            alter: self.0.alter.clone(),
+            curve: EasingCurve::new(from, to, by),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Target<T> {
+    pub target: T,
+}
+
+#[derive(Clone, Copy)]
+pub struct TargetAlter<A>
+where
+    A: Alter,
+{
+    pub alter: A,
+    pub target: A::Target,
+}
+
+#[derive(Clone, Copy)]
+pub struct TargetAlterState<A>
+where
+    A: Alter,
+{
+    pub alter: A,
+    pub target: A::Target,
+    pub state: A::Value,
+}
+
+pub trait TweenBuilderExt: Eq + Hash + Clone + Send + Sync + 'static {
+    fn tween(&self) -> TweenBuilder<Target<Self>>;
+    fn tween_via<A>(&self, via: A) -> TweenBuilder<TargetAlter<A>>
+    where
+        A: Alter<Target = Self>;
+}
+impl TweenBuilderExt for Entity {
+    fn tween(&self) -> TweenBuilder<Target<Self>> {
+        TweenBuilder(Target { target: *self })
+    }
+
+    fn tween_via<A>(&self, via: A) -> TweenBuilder<TargetAlter<A>>
+    where
+        A: Alter<Target = Self>,
+    {
+        TweenBuilder(TargetAlter {
+            target: *self,
+            alter: via,
+        })
+    }
+}
