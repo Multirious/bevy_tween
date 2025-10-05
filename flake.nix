f0febb9e6fb2ab2e7a1dc0f78caeef26a1ac123a
Multirious <multirious@outlook.com>
2025-10-05 23:10:09 +0700

    Seperate rust package from buildInputs into stableRust

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay= {
+    rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -9,8 +9,10 @@
       url = "github:numtide/flake-utils";
     };
   };
-  outputs = { nixpkgs, flake-utils, ... } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           overlays = [ (import inputs.rust-overlay) ];
@@ -18,21 +20,33 @@
         };
         buildInputs = with pkgs; [
           openssl
-          udev alsa-lib-with-plugins vulkan-loader
-          xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr
-          libxkbcommon wayland
-          (rust-bin.stable."1.85.1".default.override {
-            extensions = ["rust-analyzer" "rust-src"];
-          })
+          udev
+          alsa-lib-with-plugins
+          vulkan-loader
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          libxkbcommon
+          wayland
         ];
         nativeBuildInputs = with pkgs; [
           pkg-config
         ];
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        stableRust = (
+          pkgs.rust-bin.stable."1.85.1".default.override {
+            extensions = [
+              "rust-analyzer"
+              "rust-src"
+            ];
+          }
+        );
       in
       {
         devShells.default = pkgs.mkShell {
-          inherit buildInputs nativeBuildInputs LD_LIBRARY_PATH;
+          inherit nativeBuildInputs LD_LIBRARY_PATH;
+          buildInputs = buildInputs ++ [ stableRust ];
         };
       }
     );
