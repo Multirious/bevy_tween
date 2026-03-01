46bc19019f0e68ac0145986406a568bf216b7870
Multirious <multirious@outlook.com>
2026-03-01 20:25:01 +0700

    Update docs for EaseClosurePlugin

--- src/interpolation.rs
+++ src/interpolation.rs
@@ -443,7 +443,7 @@ pub struct EaseClosurePlugin<TimeCtx = ()>
 where
     TimeCtx: Default + Send + Sync + 'static,
 {
-    /// The schedule the systems should run on.
+    /// Register all systems from this plugin to the specified schedule.
     pub schedule: InternedScheduleLabel,
     marker: PhantomData<TimeCtx>,
 }
@@ -452,7 +452,7 @@ impl<TimeCtx> EaseClosurePlugin<TimeCtx>
 where
     TimeCtx: Default + Send + Sync + 'static,
 {
-    /// Initialize this plugin's system on this schedule
+    /// Register all systems from this plugin to the specified schedule.
     pub fn in_schedule(schedule: InternedScheduleLabel) -> Self {
         Self {
             schedule,
