4963babcedd39383257403ca4bd9398d48cdab45
Multirious <multirious@outlook.com>
2026-03-01 20:12:24 +0700

    Update docs for DefaultDynInterpolatorsPlugin

--- src/interpolate.rs
+++ src/interpolate.rs
@@ -332,7 +332,7 @@ pub struct DefaultDynInterpolatorsPlugin<TimeCtx = ()>
 where
     TimeCtx: Default + Send + Sync + 'static,
 {
-    /// The schedule the systems should run on.
+    /// Register all systems from this plugin to the specified schedule.
     pub schedule: InternedScheduleLabel,
     marker: PhantomData<TimeCtx>,
 }
@@ -396,7 +396,7 @@ impl<TimeCtx> DefaultDynInterpolatorsPlugin<TimeCtx>
 where
     TimeCtx: Default + Send + Sync + 'static,
 {
-    /// Initialize this plugin's system on this schedule
+    /// Register all systems from this plugin to the specified schedule.
     pub fn in_schedule(schedule: InternedScheduleLabel) -> Self {
         Self {
             schedule,
